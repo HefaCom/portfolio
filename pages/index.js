94fac26fb42a4da2f91ce0ef768593f74998fc21
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2023-05-30 21:46:21 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -170,22 +170,22 @@ export default function Home() {
             </div>
           </div>
         </section>
-//         <section className="py-10">
-//           <div>
-//             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
-//             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-//               Since the beginning of my journey as a freelance designer and
-//               developer, I've done remote work for
-//               <span className="text-teal-500"> agencies </span>
-//               consulted for <span className="text-teal-500">startups </span>
-//               and collaborated with talanted people to create digital products
-//               for both business and consumer use.
-//             </p>
-//             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-//               I offer from a wide range of services, including brand design,
-//               programming and teaching.
-//             </p>
-//           </div>
+        <section className="py-10">
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
+          </div>
 //           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
 //             <div className="basis-1/3 flex-1 ">
 //               <Image
@@ -248,7 +248,7 @@ export default function Home() {
 //               />
 //             </div>
 //           </div>
-//         </section> 
+        </section> 
 
         <section className="">
         <h3 className="text-3xl py-1 dark:text-white ">Major Accomplishments</h3>
