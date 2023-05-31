ffb5c0f012bdfae71538da440b027cc7a4026a95
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2023-05-31 10:04:57 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
-            <h2 className="text-3xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+            <h2 className="text-6xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
             Hezron Okoko
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
@@ -77,9 +77,9 @@ export default function Home() {
                
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl ">
-              Freelancer providing  web development, desktop application outstanding Data analysis srvices. Hire me today and let's get cracking! 
+              Freelancer providing  web development, desktop application outstanding Data analysis srvices. Hire me today and let's get it cracking! 
             </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400 sm:gap-8">
             <a href="https://www.twitter.com/_hezron__" target="_blank" rel="noopener noreferrer">
               <AiFillTwitterCircle />
             </a>
