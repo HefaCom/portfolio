47d0baf065c156fddc7cc42923ded6b4db71e1c9
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2023-05-31 08:14:46 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -187,68 +187,68 @@ export default function Home() {
               programming and teaching.
             </p>
           </div>
-//           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-//             <div className="basis-1/3 flex-1 ">
-//               <Image
-//                 className="rounded-lg object-cover"
-//                 width={"100%"}
-//                 height={"100%"}
-//                 layout="responsive"
-//                 src={web1}
-//                 alt="TheNerd"
-//               />
-//             </div>
-//             <div className="basis-1/3 flex-1">
-//               <Image
-//                 className="rounded-lg object-cover"
-//                 width={"100%"}
-//                 height={"100%"}
-//                 layout="responsive"
-//                 src={web2}
-//                 alt="TheNerd"
-//               />
-//             </div>
-//             <div className="basis-1/3 flex-1">
-//               <Image
-//                 className="rounded-lg object-cover"
-//                 width={"100%"}
-//                 height={"100%"}
-//                 layout="responsive"
-//                 src={web3}
-//                 alt="TheNerd"
-//               />
-//             </div>
-//             <div className="basis-1/3 flex-1">
-//               <Image
-//                 className="rounded-lg object-cover"
-//                 width={"100%"}
-//                 height={"100%"}
-//                 layout="responsive"
-//                 src={web4}
-//                 alt="TheNerd"
-//               />
-//             </div>
-//             <div className="basis-1/3 flex-1">
-//               <Image
-//                 className="rounded-lg object-cover"
-//                 width={"100%"}
-//                 height={"100%"}
-//                 layout="responsive"
-//                 src={web5}
-//                 alt="TheNerd"
-//               />
-//             </div>
-//             <div className="basis-1/3 flex-1">
-//               <Image
-//                 className="rounded-lg object-cover"
-//                 width={"100%"}
-//                 height={"100%"}
-//                 layout="responsive"
-//                 src={web6}
-//                 alt="TheNerd"
-//               />
-//             </div>
-//           </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1 ">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web1}
+                alt="TheNerd"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web2}
+                alt="TheNerd"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web3}
+                alt="TheNerd"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web4}
+                alt="TheNerd"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web5}
+                alt="TheNerd"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web6}
+                alt="TheNerd"
+              />
+            </div>
+          </div>
         </section> 
 
         <section className="">
