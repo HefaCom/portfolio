89dea8d78447471a1552a6ce661fea75c42994ac
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2023-05-31 11:29:31 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -210,6 +210,61 @@ export default function Home() {
             </div>
           </div>
         </section>
+         <section className="">
+        <h3 className="text-3xl py-1 dark:text-white ">Major Accomplishments</h3>
+          
+          <ol class="items-center sm:flex">
+              <li class="relative mb-6 sm:mb-0">
+                  <div class="flex items-center">
+                      <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                          <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                      </div>
+                      <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                  </div>
+                  <div class="mt-3 sm:pr-8">
+                      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">As Data Analyst </h3>
+                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">December 2, 2017</time>
+                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Developed and implemented a data-driven strategy that resulted in a 20% increase in customer retention for a leading e-commerce company.</p>
+                      <hr className="py-2"></hr>
+                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">June 20, 2022</time>
+                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Created interactive dashboards and reports that provided actionable insights to stakeholders, improving decision-making processes and driving revenue growth.</p>
+                  </div>
+              </li>
+              <li class="relative mb-6 sm:mb-0">
+                  <div class="flex items-center">
+                      <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                          <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                      </div>
+                      <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                  </div>
+                  <div class="mt-3 sm:pr-8">
+                      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">As Developer</h3>
+                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">December 23, 2021</time>
+                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Led the development of a web application from conception to deployment, incorporating user feedback and iterative improvements, resulting in a user base growth of 50% within six months.</p>
+                      <hr className="py-2"></hr>
+                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">January 20, 2021</time>
+                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Collaborated with cross-functional teams to integrate third-party APIs and streamline data workflows, enhancing system performance and reducing response time by 40%.</p>
+                  </div>
+              </li>
+              <li class="relative mb-6 sm:mb-0">
+                  <div class="flex items-center">
+                      <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                          <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                      </div>
+                      <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                  </div>
+                  <div class="mt-3 sm:pr-8">
+                      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">As a Designer</h3>
+                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">May 10, 2020</time>
+                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Designed and launched a visually stunning and user-friendly mobile app, receiving positive feedback from users and increasing user engagement by 25%.</p>
+                      <hr className="py-2"></hr>
+                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 15, 2022</time>
+                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Created a comprehensive style guide and design system, ensuring brand consistency across multiple platforms and enabling efficient collaboration with developers and stakeholders.</p>
+                  </div>
+              </li>
+          </ol>
+
+        </section>
         <section className="py-10">
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
@@ -290,61 +345,7 @@ export default function Home() {
           </div>
         </section> 
 
-        <section className="">
-        <h3 className="text-3xl py-1 dark:text-white ">Major Accomplishments</h3>
-          
-          <ol class="items-center sm:flex">
-              <li class="relative mb-6 sm:mb-0">
-                  <div class="flex items-center">
-                      <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                          <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                      </div>
-                      <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                  </div>
-                  <div class="mt-3 sm:pr-8">
-                      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">As Data Analyst </h3>
-                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">December 2, 2017</time>
-                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Developed and implemented a data-driven strategy that resulted in a 20% increase in customer retention for a leading e-commerce company.</p>
-                      <hr className="py-2"></hr>
-                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">June 20, 2022</time>
-                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Created interactive dashboards and reports that provided actionable insights to stakeholders, improving decision-making processes and driving revenue growth.</p>
-                  </div>
-              </li>
-              <li class="relative mb-6 sm:mb-0">
-                  <div class="flex items-center">
-                      <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                          <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                      </div>
-                      <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                  </div>
-                  <div class="mt-3 sm:pr-8">
-                      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">As Developer</h3>
-                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">December 23, 2021</time>
-                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Led the development of a web application from conception to deployment, incorporating user feedback and iterative improvements, resulting in a user base growth of 50% within six months.</p>
-                      <hr className="py-2"></hr>
-                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">January 20, 2021</time>
-                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Collaborated with cross-functional teams to integrate third-party APIs and streamline data workflows, enhancing system performance and reducing response time by 40%.</p>
-                  </div>
-              </li>
-              <li class="relative mb-6 sm:mb-0">
-                  <div class="flex items-center">
-                      <div class="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                          <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                      </div>
-                      <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                  </div>
-                  <div class="mt-3 sm:pr-8">
-                      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">As a Designer</h3>
-                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">May 10, 2020</time>
-                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Designed and launched a visually stunning and user-friendly mobile app, receiving positive feedback from users and increasing user engagement by 25%.</p>
-                      <hr className="py-2"></hr>
-                      <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 15, 2022</time>
-                      <p class="text-base font-normal text-gray-500 dark:text-gray-400">Created a comprehensive style guide and design system, ensuring brand consistency across multiple platforms and enabling efficient collaboration with developers and stakeholders.</p>
-                  </div>
-              </li>
-          </ol>
-
-        </section>
+       
         
 {/* My testinomials */}
         <section className="my-6">
