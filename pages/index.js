6b6718f051bdaefbd1df5276ddc418ab1c1bb263
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2023-05-31 08:18:44 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -105,14 +105,14 @@ export default function Home() {
             </div>
 
 
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-80 md:w-80">
               <Image src={deved} layout="fill" objectFit="cover" alt="TheNerd"/>
             </div>
           </div>
         </section>
         <section>
           <div>
-            <h3 className="text-3xl dark:text-white ">Services I do offer</h3>
+            <h3 className="text-3xl dark:text-white ">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance Data analyst, designer and
               developer, I've done remote work for
