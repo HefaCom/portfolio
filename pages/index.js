b1dc45ce67b45ab631b63853837726a3f48d3581
Hezron Okoko <86779873+HefaCom@users.noreply.github.com>
2023-06-11 02:42:26 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function Home() {
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl ">
               
               Highly motivated and detail-oriented data scientist with a strong background in applied statistics, data analytics, and programming.
-              Having vast skills and knowledge in statistical analysis, data visualization, and machine learning.<br>
+              Having vast skills and knowledge in statistical analysis, data visualization, and machine learning.<br></br>
                 Hire me today and let's get it cracking! 
             </p>
             <div className="text-5xl flex justify-center gap-8 py-3 text-gray-600 dark:text-gray-400">
